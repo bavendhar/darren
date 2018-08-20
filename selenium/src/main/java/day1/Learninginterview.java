6d169c49f1ae79aed17d4b544927da3dcddf575f
bavendhar <bavendhar@DESKTOP-7K4NGQ6>
2018-08-20 15:37:36 +0530

    darren

---
+++ selenium/src/main/java/day1/Learninginterview.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Learninginterview {
+	
+	public static void main(String[] args) {
+		
+		String txt="i am the winner";
+		char[] charArray = txt.toCharArray();
+		
+		
+	}
+
+}
